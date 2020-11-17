c78f32b113c1c4a385113d166de4551a6b7f8a24
Jakub Koczaja <kuba.koczaja@gmail.com>
2020-11-17 17:44:00 +0100

    login added

---
+++ src/main/java/com/dit/travel_agency/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.dit.travel_agency.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @SuppressWarnings("deprecated")
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("test").password("test").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/","/index","/home","/hotelView","/tripList","/login")
+                .permitAll()
+                .antMatchers("/addHotel","/addTrip")
+                .hasAnyAuthority("ROLE_ADMIN")
+                .antMatchers("/tripPurchase")
+                .hasAnyAuthority("ROLE_USER")
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .loginProcessingUrl("/login")
+                .failureUrl("/login?error")
+                .defaultSuccessUrl("/index")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login");
+    }
+}
